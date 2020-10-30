1c0c362cd04316e6ed9e4e03550127688e037c02
jjmontalban <jjmontalban@gmail.com>
2020-10-30 16:21:35 +0100

    Vue: CustomersComponent

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,17 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <link rel="stylesheet" href="{{ asset('public/css/app.css') }}">
+
         <title>Componentes</title>
     </head>
     <body>
+        <div id="app">
+
+            <customers></customers>
         
+        </div>
+
+        <script src="{{ asset('public/js/app.js') }}"></script>
     </body>
 </html>
